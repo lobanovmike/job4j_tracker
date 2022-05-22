66254299c39aacb96930c4474cd3507e4ce51c4e
Mikhail Lobanov <lobanov.mike@gmail.com>
2022-05-22 10:26:52 +0300

    1.6. Взаимодействие объектов [#166087 #303056]

--- src/main/java/ru/job4j/oop/BallStory.java
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -1,10 +1,5 @@
 package ru.job4j.oop;
 
-import ru.job4j.oop.Ball;
-import ru.job4j.oop.Fox;
-import ru.job4j.oop.Hare;
-import ru.job4j.oop.Wolf;
-
 public class BallStory {
     public static void main(String[] args) {
         Ball ball = new Ball();
