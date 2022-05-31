56f0428f437cd47c2d0b850c2373c801ab124560
Mikhail Lobanov <lobanov.mike@gmail.com>
2022-05-31 06:50:20 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,45 +1,4 @@
 package ru.job4j.tracker;
 
-import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-
 public class StartUITest {
-    @Test
-    public void whenCreateItem() {
-        String[] answers = {"Fix PC"};
-        Tracker tracker = new Tracker();
-        Input input = new StubInput(answers);
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item excepted = new Item("Fix PC");
-        assertThat(created.getName(), is(excepted.getName()));
-    }
-
-    @Test
-    public void whenEditItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-          String.valueOf(item.getId()),
-          "edited item"
-        };
-        StartUI.editItem(new StubInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName(), is("edited item"));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId())
-        };
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertNull(deleted);
-    }
 }
