9f75aedbe5e5e71adb92276f6cfea0ff75fb38dc
Mikhail Lobanov <lobanov.mike@gmail.com>
2022-06-03 15:13:32 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -23,4 +23,26 @@ public class StartUITest {
                         + "0. Exit Program" + System.lineSeparator()
         ));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{
+                "10", "0"
+        });
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[] {
+                new ExitAction()
+        };
+        String ln = System.lineSeparator();
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                        "Menu." + ln
+                                + "0. Exit Program" + ln
+                                + "Wrong input, you can select: 0 .. 0" + ln
+                                + "Menu." + ln
+                                + "0. Exit Program" + ln
+                )
+        );
+    }
 }
