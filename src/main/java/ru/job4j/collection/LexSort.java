17cdd3f7978be1bc8082171d825622263d688203
Mikhail Lobanov <lobanov.m1ke@yandex.ru>
2022-06-25 18:00:33 +0300

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,10 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return Integer.compare(Integer.parseInt(o1.split(". ")[0]), Integer.parseInt(o2.split(". ")[0]));
+    }
+}
