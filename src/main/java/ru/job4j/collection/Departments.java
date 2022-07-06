5a5ef35a50f639bfb9a445cd9210daae3d664c1b
Mikhail Lobanov <lobanov.m1ke@yandex.ru>
2022-07-06 16:43:19 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            String[] branches = value.split("/");
+            for (String el : branches) {
+                start = el.equals(branches[0]) ? el : start + "/" + el;
+                tmp.add(start);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+}
