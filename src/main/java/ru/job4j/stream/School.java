af5878c1678152d11b49f3dadbfd1b9c98824b98
Mikhail Lobanov <lobanov.m1ke@yandex.ru>
2022-07-17 17:33:58 +0300

    1. Фильтрация учеников [#110058 #322895]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
-    List<Student> collect(List<Student> students, Predicate<Student> predict) {
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -20,7 +20,7 @@ public class SchoolTest {
                 new Student(90, "Surname9")
         );
         School sc = new School();
-        Predicate<Student> pr = student -> (student.getScore() >= 70) && (student.getScore() <= 100);
+        Predicate<Student> pr = student -> student.getScore() >= 70 && student.getScore() <= 100;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(70, "Surname7"));
@@ -56,7 +56,7 @@ public class SchoolTest {
                 new Student(90, "Surname9")
         );
         School sc = new School();
-        Predicate<Student> pr = student -> student.getScore() >= 0 && student.getScore() <= 50;
+        Predicate<Student> pr = student -> student.getScore() > 0 && student.getScore() < 50;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(10, "Surname1"));
